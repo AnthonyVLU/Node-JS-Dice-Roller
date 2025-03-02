bbc46abac40100e31784dc13951fd59110a4c96e
AnthonyVLU <anthonyjviglielmo@lewisu.edu>
2025-03-02 14:21:40 -0600

    added html

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.use(express.static(__dirname));
 //app.use(cors());
 app.use(cors({ origin: '*' }))
 
+// Serve index.html by default for the root route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Serve index.html
+});
+
 app.get("/roll", (req, res) => {
     let rollResult = Math.floor(Math.random() * 6) + 1
     res.send(rollResult.toString()); // Sending the number as plain text
