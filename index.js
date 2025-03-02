a2e3fb51f8eced68772553236909eee62006e7a0
AnthonyVLU <anthonyjviglielmo@lewisu.edu>
2025-03-02 14:58:29 -0600

    Cors change

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000
 
-
-app.use(express.static(__dirname));
-app.use(cors());
+app.use(cors({ origin: '*' }))
 
 app.get("/roll", (req, res) => {
     let rollResult = Math.floor(Math.random() * 6) + 1
