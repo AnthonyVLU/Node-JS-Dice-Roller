83b5d75a93d6e35876c6bcb57753929141859e97
AnthonyVLU <anthonyjviglielmo@lewisu.edu>
2025-03-02 14:50:29 -0600

    Port stuff

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const express = require("express");
 const cors = require("cors"); 
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000
 
 
 app.use(express.static(__dirname));
 app.use(cors());
-//app.use(cors({ origin: '*' }))
-
-// Serve index.html by default for the root route
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Serve index.html
-});
 
 app.get("/roll", (req, res) => {
     let rollResult = Math.floor(Math.random() * 6) + 1
     res.send(rollResult.toString()); // Sending the number as plain text
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
